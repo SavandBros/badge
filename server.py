73f1d0aae7b9971d4e306515788332adc65f7060
Alireza Savand <alireza.savand@gmail.com>
2016-02-12 22:21:01 +0400

    Add server to server
    What else it should do ?

---
+++ server.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import mimetypes
+import os
+
+from badge import app
+from basement.settings import FILE_CACHE
+
+if not os.path.exists(FILE_CACHE):
+    print("Creating {0} cache dir".format(FILE_CACHE))
+    os.makedirs(FILE_CACHE)
+
+resource = app.resource
+
+if __name__ == '__main__':
+    if not os.path.exists(FILE_CACHE):
+        os.mkdir(FILE_CACHE)
+    if '.svg' not in mimetypes.types_map:
+        mimetypes.add_type("image/svg+xml", ".svg")
+
+    app.run("localhost", 8080)
