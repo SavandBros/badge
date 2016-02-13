66b284ce5a5b8bda72619bf5916cdece6a70ea00
Alireza Savand <alireza.savand@gmail.com>
2016-02-13 20:05:41 +0400

    Add more consts

--- basement/settings.py
+++ basement/settings.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+
 from redis import Redis
 
 PYPI_URL = "https://pypi.python.org/pypi/%s/json"
@@ -7,5 +9,12 @@ SHIELD_URL = "http://img.shields.io/badge/%s-%s-%s.%s"
 FILE_CACHE = "/tmp/badge.py/"
 CACHE_TIME = (60 * 60) * 24  # 24 hours
 REDIS_EXPIRE = 60 * 10  # 10 minutes
+BASE_DIR = PROJECT_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(__file__),
+    os.path.pardir)
+)
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates/')
 
 redis = Redis()
