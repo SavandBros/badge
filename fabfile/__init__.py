0f02e381e6ecc6c3332702fc285c40e595e544bc
Alireza Savand <alireza.savand@gmail.com>
2016-02-14 20:24:16 +0400

    Init on our beloved fab

---
+++ fabfile/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+fabfile
+=======
+Fabric is my fabric friend.
+
+Good things to save human life.
+"""
+
+from .fab_config import *
+from .fab_deployment import *
+from .fab_development import *
+from .fab_git import *
+from .utils import *
