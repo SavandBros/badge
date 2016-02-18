1de77915ca0deb8381194ec8b77b81808627998f
amirm3hdi <amirm3hdi@gmail.com>
2016-02-18 23:46:28 +0400

    Add append for html

--- static/js/app.js
+++ static/js/app.js
@@ -20,34 +20,55 @@ function get_badge_url(service_index, action_index, format_index, package) {
 
     return 'http://badge.kloud51.com/'+services[service_index].slug+'/'+actions[action_index].slug+'/'+package+'/badge.'+formats[format_index];
 }
+
+function append_action(service_index, action_index, format_index, package) {
+
+    var title = actions[action_index].title;
+    var url   = get_badge_url(service_index, action_index, format_index, package);
+
+    $('.actions').append('\
+        <div class="panel panel-default panel-body">\
+            <p class="col-md-2">'+title+'</p>\
+            <span class="col-md-8">\
+                <input onclick="this.select()" class="form-control badge-url" readonly value="'+url+'">\
+            </span>\
+            <span class="col-md-2">\
+                <img class="pull-right badge-image" src="'+url+'">\
+            </span>\
+        </div>\
+    ');
+}
+
+function append_service(service_index) {
+
+    $('.services').append('<li role="presentation" class="active"><a class="bg-me">'+services[service_index].title+'</a></li>');
+}
+
 function search() {
 
     event.preventDefault();
 
-    var name = $('input.search').val();
+    var package = $('input.search').val();
 
     $('input.search').val('');
-    $(".panel").css('opacity', 0.4);
+    $('.panel').css('opacity', 0.4);
     $('.badge-image').attr('src', '');
 
     $.ajax({
-        url: 'https://pypi.python.org/pypi/'+name+'/json',
+        url: 'https://pypi.python.org/pypi/'+package+'/json',
         type: 'GET',
         success: function(result) {
-            $(".panel").css('opacity', 1);
-            $(".panel").each(function(index) {
-                var url = 'http://badge.kloud51.com/pypi/'+params[index]+'/'+name+'/badge.png';
-                $($('.badge-url')[index]).val(url);
-                $($('.badge-image')[index]).attr('src', url);
-            });
+            $('.panel').css('opacity', 1);
+            reinitialize(0, package);
         },
         error: function(result) {
-            $(".panel").css('opacity', 1);
-            return alert('Invalid package name.');
+            $('.panel').css('opacity', 1);
+            reinitialize(0, 'html2text');
+            alert('Invalid package name.');
         }
     });
+}
 
-    return false;
 function reinitialize(service_index, package) {
 
     $('.actions').html('');
