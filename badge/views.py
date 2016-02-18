5a09cba1a685cb99c54ab54c2c2f43a0147e89ba
Alireza Savand <alireza.savand@gmail.com>
2016-02-18 20:06:07 +0400

    Refactor generator to service

--- badge/views.py
+++ badge/views.py
@@ -22,8 +22,8 @@ def static(request):
     return File(settings.STATIC_DIR)
 
 
-@app.route('/<string:service>/<string:generator>/<string:package>/badge.<string:extension>')
-def service_badge(request, service, generator, package, extension):
+@app.route('/<string:service>/<string:action>/<string:package>/badge.<string:extension>')
+def service_badge(request, service, action, package, extension):
     gc.collect()
 
     if extension not in settings.ALLOWED_EXTENSIONS:
@@ -36,9 +36,9 @@ def service_badge(request, service, generator, package, extension):
         request.setResponseCode(401)
         return "{} is not a valid service.".format(service)
 
-    if generator not in service_reg['actions']:
+    if action not in service_reg['actions']:
         request.setResponseCode(401)
-        return "{} is not a valid action.".format(generator)
+        return "{} is not a valid action.".format(action)
 
     ext = mimetypes.types_map[".{0}".format(extension)]
     request.headers.update({'content-type': ext})
@@ -47,7 +47,7 @@ def service_badge(request, service, generator, package, extension):
     service_class = service_class(package, format=extension, extra_context=request.args)
     """:type service_class: service.base.ServiceBase"""
     service_class.pull_package_data()
-    getattr(service_class, service_reg['actions'][generator])()
+    getattr(service_class, service_reg['actions'][action])()
     img = service_class.draw_badge()
 
     return img
