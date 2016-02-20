fb8df3df94512ce10a5aa6b85574b3dffb4a993b
Alireza Savand <alireza.savand@gmail.com>
2016-02-20 15:40:13 +0400

    Set pypi intializers and loading its dump data

--- tests/service/test_pypi_service.py
+++ tests/service/test_pypi_service.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
+from yarg import json2package
+
+from basement.utils import get_file_path_from_base
+from painter import settings
+from service import PyPiService
+
 
 class TestPyPiService(TestCase):
     package_name = "html2text"
+    pypi = PyPiService(package_name)
+    pypi.package_data = json2package(open(
+        get_file_path_from_base('tests/service/dump_data/pypi_html2text.json')
+    ).read())
