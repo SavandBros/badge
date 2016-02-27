72e69dbda40392bf08d3bbd63e2f66a9ce059c0e
Alireza Savand <alireza.savand@gmail.com>
2016-02-27 18:40:47 +0400

    Add badge_markwhat

--- badge/views.py
+++ badge/views.py
@@ -16,9 +16,49 @@ app = Klein()
 def index(request):
     return render_template('index.html')
 
+
 @app.route("/bits/static/", branch=True)
 def static(request):
     return File(settings.STATIC_DIR)
+
+
+@app.route('/<string:service>/<string:package>/<string:markwhat>/')
+def badge_markwhat(request, service, package, markwhat):
+    gc.collect()
+    markwhat = markwhat.lower()
+
+    if markwhat not in settings.MARKWAHT_TEMPLATES:
+        request.setResponseCode(400)
+        return "{} is not a markwhat.".format(markwhat)
+
+    service_reg = service_registry.services.get(service, None)
+
+    if not service_reg:
+        request.setResponseCode(400)
+        return "{} is not a valid service.".format(service)
+
+    badge_k51 = 'http://badge.kloud51.com'
+    markwhat_template = settings.MARKWAHT_TEMPLATES[markwhat]
+    markwhat_list = []
+    service_actions = service_reg['actions']['actions']
+    """:type service_actions: dict of (dict of str)"""
+    service_class = service_reg['class'](package)
+    """:type service_class: service.base.ServiceBase"""
+
+    for k, v in service_actions.items():
+        markwhat_list.append(markwhat_template.format(
+            service_display=service_class.display_name,
+            action_display=v['display_name'],
+            action=v['short_name'],
+            service=service,
+            pkg_name=package,
+            pkg_url=service_class.get_package_url(),
+            badge_k51=badge_k51,
+        ))
+
+    request.headers.update({'content-type': 'txt'})
+
+    return '\n'.join(markwhat_list)
 
 
 @app.route('/<string:service>/<string:action>/<string:package>.<string:extension>')
